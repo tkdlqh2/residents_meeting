4c5d93185e623d67124d44572c5d8cc2a7f44f53
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-08 23:42:47 +0900

    Initial commit

---
+++ src/test/java/com/example/residents_meeting/ResidentsMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.residents_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentsMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
