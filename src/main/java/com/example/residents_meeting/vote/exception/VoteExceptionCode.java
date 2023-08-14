ad68f4b9e1266c2a5fc21ecfa5e50689c0affd59
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-14 18:49:37 +0900

    exception code 추가

--- src/main/java/com/example/residents_meeting/vote/exception/VoteExceptionCode.java
+++ src/main/java/com/example/residents_meeting/vote/exception/VoteExceptionCode.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum VoteExceptionCode {
+	AGENDA_NOT_FOUND(404, "Agenda not found"),
 	SELECT_OPTION_NOT_FOUND(404, "Select option not found"),
+	ONGOING_SECRET_VOTE(400, "This agenda is ongoing secret vote"),
 	NO_RIGHT_FOR_VOTE(400, "This user cannot vote for this agenda");
 
 	VoteExceptionCode(int statusCode, String message) {
