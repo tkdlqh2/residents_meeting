be272c68483b4ca23b18d4b8843625ecdcaf10bd
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-16 15:09:31 +0900

    repository test 를 위한 기본 class 들과 sql 파일 추가

---
+++ src/test/java/com/example/residents_meeting/vote/repository/SelectOptionRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.residents_meeting.vote.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectOptionRepositoryTest {
+
+	@Test
+	void countById() {
+	}
+
+	@Test
+	void findUserIdsByAgendaIdAndId() {
+	}
+}
---
+++ src/test/java/com/example/residents_meeting/vote/repository/VoteRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.residents_meeting.vote.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoteRepositoryTest {
+
+	@Test
+	void findVoteHistoryByUserIdAndAgendaId() {
+	}
+}
