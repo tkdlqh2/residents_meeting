16ab9088c505df10fc72547e9a9bb90597cbe9b4
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-20 06:12:05 +0900

    GlobalExceptionHandler 에 exception 추가

--- src/main/java/com/example/residents_meeting/common/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/residents_meeting/common/controller/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.residents_meeting.user.exception.UserException;
 import com.example.residents_meeting.vote.exception.VoteException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,18 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> handleException(Exception e) {
 		log.error("Exception: ", e);
 		return ResponseEntity.status(500).body("Internal Server Error");
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+		log.error("IllegalArgumentException: ", e);
+		return ResponseEntity.status(400).body(e.getMessage());
+	}
+
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException e) {
+		log.debug("UsernameNotFoundException: ", e);
+		return ResponseEntity.status(404).body(e.getMessage());
 	}
 
 	@ExceptionHandler(UserException.class)
