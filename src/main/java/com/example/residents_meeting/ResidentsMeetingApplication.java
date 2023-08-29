7663219fa8f9489382b3947c01c470891fb22b74
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-29 21:21:23 +0900

    ResidentsMeetingApplication 은 Eureka Server 로 변경

--- src/main/java/com/example/residents_meeting/ResidentsMeetingApplication.java
+++ src/main/java/com/example/residents_meeting/ResidentsMeetingApplication.java
@@ -2,10 +2,10 @@ package com.example.residents_meeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableEurekaServer
 public class ResidentsMeetingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/residents_meeting/vote/domain/dto/AgendaCreationResultDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.residents_meeting.vote.domain.dto;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public record AgendaCreationResultDTO(String apartmentCode, String title, String details, LocalDate endDate, List<String> selectOptionSummaryList) {
-}
