b3bdf9b57203fbe4078c729a83487e080496b1bf
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-08-30 17:38:41 +0900

    config Application 추가

--- api_gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,8 +1,11 @@
 package com.example.api_gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
