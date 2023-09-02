b61a263002230902e121c2057c1a3498de353248
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-09-02 18:25:09 +0900

    VoteServiceImplTest 추가 및 관련 사항 변경

--- vote_service/src/main/java/com/example/vote_service/domain/dto/SelectOptionVo.java
+++ vote_service/src/main/java/com/example/vote_service/domain/dto/SelectOptionVo.java
@@ -1,6 +1,5 @@
 package com.example.vote_service.domain.dto;
 
-import com.example.vote_service.domain.Agenda;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -8,7 +7,7 @@ import java.time.LocalDateTime;
 @Builder
 public record SelectOptionVo(
 		Long id,
-		Agenda agenda,
+		Long agendaId,
 		String summary,
 		String details,
 		LocalDateTime createdAt,
--- vote_service/src/main/java/com/example/vote_service/domain/dto/VoteCreationDto.java
+++ vote_service/src/main/java/com/example/vote_service/domain/dto/VoteCreationDto.java
@@ -1,4 +1,4 @@
 package com.example.vote_service.domain.dto;
 
-public record VoteCreationDto(Long agendaId, Long selectOptionId, Long userId) {
+public record VoteCreationDto(Long selectOptionId) {
 }
